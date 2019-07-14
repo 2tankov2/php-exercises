9d2fb70c470359f57e8aaaa93cdbea54bdeaab93
2tankov2 <domino-82@mail.ru>
2019-07-14 16:33:28 +0300

    isPalindrome

---
+++ isPolindrome.php
@@ -0,0 +1,39 @@
+<?php
+// Палиндром — число, слово или текст, одинаково читающееся в обоих направлениях. Например: "радар", "топот".
+namespace App\Text;
+
+// BEGIN (write your solution here)
+function isPalindrome($text)
+{
+    if (strlen($text) < 2) {
+        return true;
+    }
+    $half = (ceil(strlen($text) / 2));
+    $i = 0;
+    $j = strlen($text) - 1;
+    
+    for ($i; $i < $half; $i++) {
+        $currentChar = $text[$i];
+        $lastChar = $text[$j];
+        if ($currentChar !== $lastChar) {
+            return false;
+        }
+        $j--;
+    }
+    return true;
+}
+// END
+// BEGIN (write your solution here)
+function isPalindrome($text)
+{
+    if (strlen($text) < 2) {
+        return true;
+    }
+    $firstChar = substr($text, 0, 1);
+    $lastChar = substr($text, strlen($text) - 1, 1);
+    if ($firstChar !== $lastChar) {
+            return false;
+    }
+    return isPalindrome(substr($text, 1, strlen($text) - 2));
+}
+// END
